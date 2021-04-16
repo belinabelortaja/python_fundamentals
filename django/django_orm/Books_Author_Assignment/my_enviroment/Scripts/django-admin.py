02303baec56021bab5e6ce4d8b21246687fac2ae
belina.belortaja <belinabelortaja@yahoo.com>
2021-04-16 18:19:03 +0200

    Books and Authors Shell

---
+++ django/django_orm/Books_Author_Assignment/my_enviroment/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\python-stack\django\django_orm\books_author_assignment\my_enviroment\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
